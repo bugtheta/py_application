52ba3848173177f56573cab9d219d08af897d3d9
zzzzzzhu <bugtheta@gmail.com>
2020-04-06 17:45:00 +0800

    Update p32_get_ctrip.py

--- p32_get_ctrip.py
+++ p32_get_ctrip.py
@@ -1,9 +1,11 @@
+# work with header
 import requests
 import json
 import pandas as pd
 import datetime
 import time
 import matplotlib.pyplot as plt
+import random
 
 
 def make_request(dep_city='bjs',
@@ -11,7 +13,7 @@ def make_request(dep_city='bjs',
                  date='2020-04-04',
                  hasChild=False,
                  hasBaby=False,
-                 classType='ALL'):
+                 classType='ALL', last_ref="https://flights.ctrip.com/"):
     '''
     函数用于根据参数对ctrip的api站点发送请求并返回routeList数据
     :param dep_city:始发城市
@@ -21,26 +23,28 @@ def make_request(dep_city='bjs',
     :param hasBaby:是否有婴儿
     :param classType:舱位类型
     :return: routeList 用于paser_route
+    :return: next_referer 用于下一次的headers
     '''
+
     # 定义访问相关参数
-    referer = 'https://flights.ctrip.com/international/search/domestic'
+    referer = 'https://flights.ctrip.com/'
     api_url = 'https://flights.ctrip.com/itinerary/api/12808/products'
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36"
-                      " (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36"
+                      " (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
         "Referer": referer,
         "Content-Type": "application/json",  # 声明文本类型为 json 格式
     }
 
-    # 构建访问链接
-    new_referer = 'https://flights.ctrip.com/itinerary/oneway/'
-    yesterday = str(pd.to_datetime(date).date() + datetime.timedelta(days=-1))
-    new_referer += dep_city + '_' + arr_city + '?' + 'date=' + yesterday + \
-                   '&hasChild=' + str(hasChild) + '&hasBaby=' + str(hasBaby) + \
-                   '&classType=' + classType
+    this_date = str(pd.to_datetime(date).date())
+
+    next_referer = 'https://flights.ctrip.com/itinerary/oneway/'
+    next_referer += dep_city + '-' + arr_city + '?' + 'date=' + this_date
+    print("last ref", last_ref)
+    print("new ref", next_referer)
 
     # 载入请求json并根据搜索条件更新数据
-    re_json = json.load(open('request.json', "r"))
+    re_json = json.load(open('./json/request.json', "r"))
 
     re_json['date'] = date
     re_json['hasChild'] = hasChild
@@ -49,19 +53,19 @@ def make_request(dep_city='bjs',
     re_json['airportParams'][0]['acity'] = arr_city
     re_json['airportParams'][0]['date'] = date
 
-    headers['Referer'] = new_referer
+    headers['Referer'] = last_ref
 
     # 利用访问数据对API站点进行访问，获取返回数据
     r = requests.post(api_url, data=json.dumps(re_json), headers=headers)
     respone_json = r.json()
 
-    json_dir = 'response.json'
+    json_dir = './json/response.json'
 
     # 从返回结果数据中提取route出列表
     data = respone_json['data']
     routeList = data['routeList']
 
-    return routeList
+    return routeList, next_referer
 
 
 def route_parser(routeList):
@@ -77,15 +81,15 @@ def route_parser(routeList):
     for i, route0 in enumerate(routeList):
 
         legs = route0['legs']
-        print("方案%d/%d)" % (i, len(routeList)))
+        print("方案%d/%d" % (i, len(routeList)))
 
         # 遍历所有leg
         for j, legs0 in enumerate(legs):
-            print(" 航节 %d" % j)
+            print("  航节%d" % j)
 
             # 跳过非航班的leg
             if 'flightId' not in legs0:
-                print("这个leg不是飞机")
+                print("  这个leg不是飞机")
                 continue
 
             # 航班信息
@@ -101,8 +105,8 @@ def route_parser(routeList):
             departureDate = flight['departureDate']
             arrivalDate = flight['arrivalDate']
 
-            print("  航班%s %s-%s" % (flightNumber, dep_airport, arr_airport))
-            print("  %s起飞 %s到达" % (departureDate[:-3], arrivalDate[:-3]))
+            print("        航班%s %s-%s" % (flightNumber, dep_airport, arr_airport))
+            print("        %s起飞 %s到达" % (departureDate[:-3], arrivalDate[:-3]))
 
             # 舱位信息
             cabins = legs0['cabins']
@@ -128,62 +132,84 @@ def route_parser(routeList):
 
             # 存储下所有的最低母舱位的价格信息
             for l, cls in enumerate(min_price.keys()):
-                print("  %s舱(%s)最低价格 %d元" % (cls, min_cabinClass[cls], min_price[cls]))
+                print("        %s舱(%s)最低价格 %d元" % (cls, min_cabinClass[cls], min_price[cls]))
                 class_lines.append([flightNumber, dep_airport, arr_airport, departureDate[:-3], arrivalDate[:-3], cls,
                                     min_cabinClass[cls], min_price[cls]])
 
-    keys = ['flight_no', 'dep', 'arr', 'dep_date', 'arr_date', 'class', 'sub_class', 'price']
+    keys = ['flight_no', 'dep', 'arr', 'dep_date', 'arr_date', 'cabin_class', 'price_class', 'price']
     class_df = pd.DataFrame(class_lines, columns=keys)
 
     return class_df
 
 
-def draw_price(df, title='lowest price in PEK-CAN'):
+def draw_price_carrier(df, dep, arr, tw0=8, tw1=10):
     df = df.copy()
+
+    title = 'lowest price in %s-%s [%d,%d)' % (dep, arr, tw0, tw1)
+
+    df = df.loc[df['arr'] == arr]
+    df = df.loc[df['dep'] == dep]
+
+    df = df.loc[pd.to_datetime(df.dep_date).dt.hour < tw1]
+    df = df.loc[pd.to_datetime(df.dep_date).dt.hour >= tw0]
+
     df['carrier'] = df['flight_no'].agg(lambda x: x[:2])
     df['dep_date'] = pd.to_datetime(df['dep_date']).dt.date
-    y_class_df = df.loc[df['class'] == 'Y']
+    y_class_df = df.loc[df['cabin_class'] == 'Y']
     min_df = y_class_df.groupby(['dep_date', 'carrier'])['price'].min()
     min_df = min_df.unstack()
 
-    lg = ['CZ', 'CA', 'HU', 'MU']
+    lg = df['carrier'].unique()
+    print(lg)
+
     plt.figure(figsize=(8, 5))
-    plt.plot(min_df.index, min_df['CZ'], 'o:', markerfacecolor='white', color='C0')
-    plt.plot(min_df.index, min_df['CA'], 'o:', markerfacecolor='white', color='C3')
-    plt.plot(min_df.index, min_df['HU'], 'o:', markerfacecolor='white', color='C1')
-    plt.plot(min_df.index, min_df['MU'], 'o:', markerfacecolor='white', color='C2')
+    for cr in lg:
+        plt.plot(min_df.index, min_df[cr], 'o:', markerfacecolor='white')
 
     plt.legend(labels=lg, loc='best')
-    plt.xticks(rotation=15)
     plt.title(title)
 
     plt.show()
 
 
-# 取未来7到20天起飞的日期
-f7_dates = [datetime.datetime.now().date() +
-            datetime.timedelta(days=x) for x in range(7, 21)]
-f7_dates = [str(d) for d in f7_dates]
+def get_future_data(day_begin=1, day_end=30, dep_city='bjs', arr_city='can'):
+    # 取未来1到30天起飞的日期
+    f7_dates = [datetime.datetime.now().date() +
+                datetime.timedelta(days=x) for x in range(day_begin, day_end + 1)]
+    f7_dates = [str(d) for d in f7_dates]
 
-date_list = []
+    date_list = []
 
-# 循环更新参数并进行请求 获得返回值
-for i, date in enumerate(f7_dates):
-    print("crawling the %s  %d/%d" % (date, i, len(f7_dates)))
-    # 输入参数进行请求
-    r_list = make_request(date=date)
-    # 对返回数据进行解析提取
-    date_df = route_parser(routeList=r_list)
-    # 将每个日期的DataFrame存入到list
-    date_list.append(date_df)
-    time.sleep(1.3333)
+    ref = "https://flights.ctrip.com/"
 
-# 对多个日期数据进行连接
-f7_df = pd.concat(date_list, axis=0)
+    # 循环更新参数并进行请求 获得返回值
+    for i, date in enumerate(f7_dates):
+        print("crawling the %s  %d/%d" % (date, i, len(f7_dates)))
+        # 输入参数进行请求
+        r_list, new_ref = make_request(date=date, dep_city=dep_city, arr_city=arr_city, last_ref=ref)
+        ref = new_ref
 
-# 存储数据
-f7_df['download_date'] = datetime.datetime.now()
-f7_df.to_csv('future_7to20days.csv', index=False)
+        # 对返回数据进行解析提取
+        date_df = route_parser(routeList=r_list)
+        # 将每个日期的DataFrame存入到list
+        date_list.append(date_df)
+        time.sleep(4.3333 + random.random() * 2)
 
-# 绘制票价图像
-draw_price(f7_df)
+    # 对多个日期数据进行连接
+    future_df = pd.concat(date_list, axis=0)
+    future_df['download_date'] = datetime.datetime.now()
+
+    return future_df
+
+
+if __name__ == '__main__':
+    trip_o = 'can'
+    trip_d = 'bjs'
+
+    data_df = get_future_data(day_begin=7, day_end=13, dep_city=trip_o, arr_city=trip_d)
+
+    # 存储数据
+    data_df.to_csv(trip_o + trip_d + '_7to13days.csv', index=False)
+
+    # 绘制票价图像
+    draw_price_carrier(data_df, dep='CAN', arr='PEK', tw0=8, tw1=10)
